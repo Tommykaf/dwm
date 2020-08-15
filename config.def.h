681bac063acfb8d05e9f67598fdc0e2923c4f3f4
Tommykaf <you@example.com>
2020-08-15 06:02:31 +0300

    Revert "added fullgaps"
    This reverts commit ce6e5aa1f4b00b733d5100906e8ef1b9b04c6bbd.

--- config.def.h
+++ config.def.h
@@ -1,7 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int gappx     = 5;        /* gaps between windows */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
@@ -10,6 +9,7 @@ static const char *fonts[]          = {
 	"FiraCode:style=Regular:pixelsize=18:antialias=true:autohint=true",
 	"Twemoji:style=Regular:pixelsize=14:antialias=true:autohint=true"
 };
+static const char dmenufont[]       = "FiraCode:style=Regular:pixelsize=18:antialias=true:autohint=true";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -126,9 +126,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
